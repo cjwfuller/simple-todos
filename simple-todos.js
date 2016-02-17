2772dbf5c98e1fc379701babed42a9ead418f549
Chris Fuller <chris@cjwfuller.com>
2016-02-17 18:49:03 +0000

    learning about client forms

--- simple-todos.js
+++ simple-todos.js
@@ -7,4 +7,30 @@ if (Meteor.isClient) {
       return Tasks.find({});
     }
   });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
+
+  Template.body.helpers({
+    tasks: function () {
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
 }
